0bf0c50860d3c16635ef546c5707dbfb3049bdd6
Vivek Kumar Sahani <itsvks19@gmail.com>
2023-03-10 10:49:51 +0530

    Update AppTheme

--- app/src/main/java/com/raredev/vcspace/activity/VCSpaceActivity.java
+++ app/src/main/java/com/raredev/vcspace/activity/VCSpaceActivity.java
@@ -3,6 +3,7 @@ package com.raredev.vcspace.activity;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.elevation.SurfaceColors;
 import com.raredev.vcspace.CrashHandler;
 
 public class VCSpaceActivity extends AppCompatActivity {
@@ -11,6 +12,7 @@ public class VCSpaceActivity extends AppCompatActivity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     Thread.setDefaultUncaughtExceptionHandler(new CrashHandler(this));
+    getWindow().setStatusBarColor(SurfaceColors.SURFACE_0.getColor(this));
     findBinding();
     setContentView(getLayout());
     onCreate();
