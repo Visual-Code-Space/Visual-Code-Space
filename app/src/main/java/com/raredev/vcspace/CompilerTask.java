06f58f4506dbc925a26e46748ecaeeb3dc54d333
Moder <125615066+moderGamer@users.noreply.github.com>
2023-03-17 12:14:40 +0330

    Update CompilerTask.java
    init Road offline Gcc

--- app/src/main/java/com/raredev/vcspace/CompilerTask.java
+++ app/src/main/java/com/raredev/vcspace/CompilerTask.java
@@ -1,6 +1,6 @@
 package com.raredev.vcspace;
 
-import android.app.ProgressDialog;
+iimportmport android.app.ProgressDialog;
 import android.content.Context;
 
 import com.blankj.utilcode.util.FileUtils;
@@ -58,27 +58,87 @@ public class CompilerTask {
                   progress++;
                   mProgressDialog.setProgress(progress);
                 }
-              } catch (IOException e) {
-                e.printStackTrace();
-                result = e.getMessage();
-              }
-              return result;
-            });
+import android.app.ProgressDialog;
+import android.content.Context;
+import com.blankj.utilcode.util.FileUtils;
+import android.os.AsyncTask;
+import android.os.Environment;
 
-    executor.execute(
-        () -> {
-          try {
-            String result = future.get();
-            mProgressDialog.dismiss();
-            onCompleteListener.onComplete(result);
-          } catch (Exception e) {
-            e.printStackTrace();
-            onCompleteListener.onComplete(e.getMessage());
-          }
-        });
-  }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-  public interface OnCompleteListener<T> {
-    void onComplete(T result);
-  }
+/*
+* How to using?
+* CompilerTask compilerTask = new CompilerTask(this);
+		compilerTask.compileCpp(filePath, new CompilerTask.OnCompleteListener<String>() {
+			@Override
+			public void onComplete(String result) {
+				// نتیجه کامپایل شده فایل C++ در اینجا قرار می گیرد
+				//Result Cpp Code
+			}
+		});
+
+*/
+
+public class CompilerTask {
+	private Context mContext;
+	private ProgressDialog mProgressDialog;
+
+	public CompilerTask(Context context) {
+		mContext = context;
+	}
+
+	public void compileCpp(String filePath, final OnCompleteListener<String> onCompleteListener) {
+		mProgressDialog = new ProgressDialog(mContext);
+		mProgressDialog.setMessage("Compiling C++ file...");
+		mProgressDialog.setIndeterminate(false);
+		mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		mProgressDialog.setCancelable(false);
+		mProgressDialog.show();
+
+		AsyncTask.execute(() -> {
+			String result = "";
+			try {
+				// Extract the external storage directory path
+				String externalStoragePath = Environment.getExternalStorageDirectory().getPath();
+
+				// Navigate to the external storage directory
+				String cmd = "cd " + externalStoragePath;
+
+				// Extract the app's media directory path
+				String mediaPath = externalStoragePath + "/Android/media/" + mContext.getPackageName();
+
+				// Extract the required files to the app's media directory
+				cmd += " && tar -xvf " + mediaPath + "/files.tar -C " + mediaPath;
+
+				// Install GCC if necessary
+				cmd += " && pkg install -y gcc";
+
+				// Compile the C++ file using GCC
+				cmd += " && g++ " + FileUtils.getFileName(filePath) + " -o "
+						+ FileUtils.getFileName(filePath).replace(".cpp", "");
+
+				// Run the compilation command and capture the output
+				Process process = Runtime.getRuntime().exec(new String[] { "sh", "-c", cmd });
+				BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				String line;
+				int progress = 0;
+				while ((line = reader.readLine()) != null) {
+					result += line + "\n";
+					progress++;
+					mProgressDialog.setProgress(progress);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				result = e.getMessage();
+			}
+			mProgressDialog.dismiss();
+			onCompleteListener.onComplete(result);
+		});
+	}
+
+	public interface OnCompleteListener<T> {
+		void onComplete(T result);
+	}
 }
