d5663dcfa1824b9577419ad5fbe30513fa8dae78
Vivek <itsvks19@gmail.com>
2023-04-30 15:32:17 +0530

    Add support for extension-api

--- app/src/main/java/com/raredev/vcspace/util/CommentSystem.java
+++ app/src/main/java/com/raredev/vcspace/util/CommentSystem.java
@@ -1,6 +1,7 @@
 package com.raredev.vcspace.util;
 
 import io.github.rosemoe.sora.text.Content;
+import io.github.rosemoe.sora.text.Cursor;
 
 public class CommentSystem {
   public static void addSingleComment(String commentPrefix, Content text, int line) {
@@ -10,20 +11,51 @@ public class CommentSystem {
     text.insert(line, 0, commentLine);
   }
 
-  public static void addBlockComment(String blockCommentOpenPrefix, String blockCommentClosePrefix, Content text, int startLine, int endLine) {
+  public static void addBlockComment(
+      String blockCommentOpenPrefix,
+      String blockCommentClosePrefix,
+      Content text,
+      int startLine,
+      int endLine) {
     // Insert multi-line comment at the beginning of the start line
     text.insert(startLine, 0, blockCommentOpenPrefix + "\n");
-
-    // Insert comment lines after the start line
-    
-    //    for (int line = startLine + 1; line < endLine; line++) {
-    //      String commentLine =
-    //          line < startLine + commentLines.length ? commentLines[line - startLine - 1] : "";
-    //
-    //      text.insert(line, 0, "// " + commentLine + "\n");
-    //    }
 
     // Insert multi-line comment end at the end of the end line
     text.insert(endLine + 2, 0, blockCommentClosePrefix + "\n");
   }
+
+/*
+  public static String uncomment(Content text) {
+    StringBuilder sb = new StringBuilder();
+    boolean inBlockComment = false;
+    int i = 0;
+
+    while (i < text.length()) {
+      char c = text.charAt(i);
+
+      if (inBlockComment) {
+        if (c == '*' && i < text.length() - 1 && text.charAt(i + 1) == '/') {
+          inBlockComment = false;
+          i++;
+        }
+      } else {
+        if (c == '/' && i < text.length() - 1 && text.charAt(i + 1) == '/') {
+          i = text.toString().indexOf('\n', i + 2);
+          if (i == -1) {
+            break;
+          }
+        } else if (c == '/' && i < text.length() - 1 && text.charAt(i + 1) == '*') {
+          inBlockComment = true;
+          i++;
+        } else {
+          sb.append(c);
+        }
+      }
+
+      i++;
+    }
+
+    return sb.toString();
+  }
+  */
 }
