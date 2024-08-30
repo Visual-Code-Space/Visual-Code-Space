00115e83aed5f874d953dd2c8324327c8bb14d73
Vivek <itsvks19@gmail.com>
2024-08-30 07:13:56 +0530

    build: update gradle to 8.8 and AGP to 8.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,11 @@ android {
     }
   }
 
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+
   compileOptions { isCoreLibraryDesugaringEnabled = true }
 
   packaging {
--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -12,6 +12,11 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
   }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
 }
 
 dependencies {
