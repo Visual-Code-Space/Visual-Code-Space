d2ba42fccb3841e56e4b6cd09cc6859c65216f79
Aquiles Trindade <devsuay@gmail.com>
2024-10-09 01:47:14 -0300

    feat: apply Josefins Sans fontFamily in Compose

--- app/src/main/java/com/teixeira/vcspace/ui/theme/Type.kt
+++ app/src/main/java/com/teixeira/vcspace/ui/theme/Type.kt
@@ -2,33 +2,98 @@ package com.teixeira.vcspace.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
+import com.teixeira.vcspace.resources.R
+
+/*
+* Tipography definition for theming
+*/
+val JosefinSansFontFamily = FontFamily(
+    Font(R.font.josefin_sans, FontWeight.Normal, FontStyle.Normal),
+)
+
+/*
+* Overrides the default typo 
+*/
 val Typography = Typography(
-  bodyLarge = TextStyle(
-    fontFamily = FontFamily.Default,
-    fontWeight = FontWeight.Normal,
-    fontSize = 16.sp,
-    lineHeight = 24.sp,
-    letterSpacing = 0.5.sp
-  )
-  /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = JosefinSansFontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 57.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = JosefinSansFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 45.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = JosefinSansFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = JosefinSansFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = JosefinSansFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = JosefinSansFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = JosefinSansFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = JosefinSansFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = JosefinSansFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = JosefinSansFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = JosefinSansFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = JosefinSansFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = JosefinSansFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = JosefinSansFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = JosefinSansFontFamily,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 11.sp
     )
-    */
 )
