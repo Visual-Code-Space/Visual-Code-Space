6a7989b6a65c48ad65d9801a9d1432b17ecede27
Vivek <itsvks19@gmail.com>
2024-12-09 00:49:24 +0530

    refactor: remove temp code as it was likely for testing purposes and is no longer needed

--- app/src/main/java/com/teixeira/vcspace/ui/screens/editor/components/EditorTopBar.kt
+++ app/src/main/java/com/teixeira/vcspace/ui/screens/editor/components/EditorTopBar.kt
@@ -79,19 +79,15 @@ import com.blankj.utilcode.util.UriUtils
 import com.downloader.Error
 import com.downloader.OnDownloadListener
 import com.downloader.PRDownloader
-import com.google.ai.client.generativeai.type.asTextOrNull
 import com.google.android.material.snackbar.Snackbar
 import com.hzy.libp7zip.P7ZipApi
 import com.teixeira.vcspace.PYTHON_PACKAGE_URL_32_BIT
 import com.teixeira.vcspace.PYTHON_PACKAGE_URL_64_BIT
-import com.teixeira.vcspace.resources.R
 import com.teixeira.vcspace.activities.Editor.LocalCommandPaletteManager
 import com.teixeira.vcspace.activities.Editor.LocalEditorDrawerState
 import com.teixeira.vcspace.activities.TerminalActivity
 import com.teixeira.vcspace.app.VCSpaceApplication
 import com.teixeira.vcspace.app.strings
-import com.teixeira.vcspace.core.Secrets
-import com.teixeira.vcspace.core.ai.Gemini
 import com.teixeira.vcspace.core.components.Tooltip
 import com.teixeira.vcspace.core.components.common.VCSpaceTopBar
 import com.teixeira.vcspace.core.settings.Settings.EditorTabs.rememberAutoSave
@@ -101,6 +97,7 @@ import com.teixeira.vcspace.keyboard.model.Command.Companion.newCommand
 import com.teixeira.vcspace.plugins.internal.PluginManager
 import com.teixeira.vcspace.preferences.pythonDownloaded
 import com.teixeira.vcspace.preferences.pythonExtracted
+import com.teixeira.vcspace.resources.R
 import com.teixeira.vcspace.ui.screens.editor.EditorViewModel
 import com.teixeira.vcspace.ui.screens.editor.components.view.CodeEditorView
 import com.teixeira.vcspace.utils.isFileRunnable
@@ -112,7 +109,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.greenrobot.eventbus.EventBus
 import java.io.File
 import java.nio.file.Files
@@ -434,18 +430,6 @@ fun EditorTopBar(
                   )
                 }
               )
-              scope.launch {
-                val response = Gemini.explainCode("""
-                  fun hello() {
-                    println("Hello, World!")
-                  }
-                """.trimIndent()
-                )
-
-                withContext(Dispatchers.Main) {
-                  ToastUtils.showLong(response.candidates[0].content.parts[0].asTextOrNull())
-                }
-              }
               showMenu = false
             },
             leadingIcon = {
