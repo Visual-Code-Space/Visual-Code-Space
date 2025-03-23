8284b96c5a3c7ff6676a99f6f05650cd208f5338
Vivek <itsvks19@gmail.com>
2025-03-23 09:43:19 +0530

    refactor: Adjust proot arguments for storage access

--- app/src/main/assets/terminal/init-host.sh
+++ app/src/main/assets/terminal/init-host.sh
@@ -1,3 +1,3 @@
-prootArgs="-r $ALPINE -0 -b /dev/ -b /sys/ -b /proc/ -b /sdcard -b /storage/emulated/0 -b $PREFIX -w /home --kill-on-exit --link2symlink"
+prootArgs="-r $ALPINE -0 -b /dev/ -b /sys/ -b /proc/ -b /sdcard -b /storage -b $PREFIX -w /home --kill-on-exit --link2symlink"
 
 exec $PROOT $prootArgs /bin/sh $PREFIX/bin/init "$@"
